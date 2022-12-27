5f1b40a45914e8c114ef90f8b55effe96a85d3be
Ratiesh Dalvi <115468621+codeRatish2@users.noreply.github.com>
2022-12-27 12:25:57 +0530

    Add files via upload

---
+++ Array-Each-num-a-print.js
@@ -0,0 +1,5 @@
+var numbers = [10,20,30,40,50,60,]
+
+for(var i= 0; numbers[i] > i; i++){
+console.log(numbers[i] + "a");
+}
---
+++ without-using-pop-function.js
@@ -0,0 +1,8 @@
+var student = ["Ratish","shivam","Archit","Raj","Ruta","Swapnali","Deepa","Dipti","Sagar","neha","Saurabh","Raghav","Samrudhi"]
+
+
+  for(var i=0; i<10 ;i++){
+    console.log(student[i]);
+  }
+
+  

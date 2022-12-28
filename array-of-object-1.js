1727e666036761ac109ec0f6a16279f53ff7ff8a
Ratiesh Dalvi <115468621+codeRatish2@users.noreply.github.com>
2022-12-28 18:48:11 +0530

    Add files via upload

---
+++ array-of-object-1.js
@@ -0,0 +1,23 @@
+let cars = [
+    {
+        color: "red",
+        type: "minivan",
+        registration : new Date ('03-01-2017'),
+        capacity : 7
+    },
+    {
+        color: "purple",
+        type: "station wagon",
+        registration: new Date('03-03-2018'),
+        capacity: 5
+    },
+    {
+        color:"white",
+        type: "cabrio",
+        registration: new Date('04-03-2018'),
+        capacity: 2
+
+    },
+
+    
+]

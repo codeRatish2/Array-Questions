21d5de8485e8cf09b93b9de6c2058d0a81d90145
Ratiesh Dalvi <115468621+codeRatish2@users.noreply.github.com>
2022-12-29 13:23:41 +0530

    Add files via upload

---
+++ array-of-nested-object .js
@@ -0,0 +1,10 @@
+const student = {
+    name :'ratish'
+    age: 26,
+    marks: {
+        science: 70,
+        maths: 75,
+    }
+}
+  
+console.log(student.marks);
---
+++ loop-using of array-of object.js
@@ -0,0 +1,8 @@
+var obj = [
+   {name: "Shivam", age: 26},
+   {name: "Ratish", age: 26},
+   {name: "Rakesh", age: 32 }
+];
+for (const key of object.keys(obj)){
+    console.log(obj[key]);
+}
